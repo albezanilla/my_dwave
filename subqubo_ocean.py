5b4a8524b6c8515b999e40ccbfff35907e54f7b6
DennyCQ <denny.dahl@coldquanta.com>
2020-12-09 16:56:02 -0700

    The Ocean BQM generator builds Ising models, so I had to include logic to convert the BQM from an Ising model to a QUBO. I was also able to simplify the construction of the NetworkX complete graph.

--- subqubo_ocean.py
+++ subqubo_ocean.py
@@ -5,6 +5,7 @@ import networkx as nx
 import sys
 import ii
 import sympy
+import sympy_util
 import pickle
 
 valid_args = {'cube1', 'cube2', 'cube3', 'cube4', 'up', 'down', 'front', 'back'}
@@ -50,37 +51,56 @@ if len(sys.argv) == 4:
             print('  %8d' % bit, end='')
         print('')
 
+    symbol_names = ['spin_' + name for name in symbol_names]
+    aux_names = ['spin_' + name for name in aux_names]
     all_names = symbol_names + aux_names
+    aux_names = [name.replace('spin_', '') for name in aux_names]
     num_vars = len(all_names)
-    graph = nx.Graph()
-    for var in all_names:
-        graph.add_node(var)
-    for edge in itertools.combinations(all_names, 2):
-        graph.add_edge(edge[0], edge[1])
+    graph = nx.complete_graph(all_names)
 
     feasible_configurations = set()
     for bool_state in valid_states:
         spin_state = tuple( (1 - 2*bool_state[i] for i in range(8)) )
         feasible_configurations.add(spin_state)
         
-    # Generate BQM:
+    # Generate BQM over Ising states:
     bqm, gap, aux = penaltymodel.mip.generation.generate_bqm(graph,
                                                              feasible_configurations,
                                                              symbol_names,
                                                              max_variables=num_vars,
                                                              return_auxiliary=True)
 
+    # Map each string name (logical and auxiliary) to a Sympy symbol:
     symbols = dict()
     for name in all_names:
         symbol = sympy.symbols(name)
         symbols[name] = symbol
 
-    subqubo = 0
+    # Iterate over the linear and quadratic terms in the BQM and for
+    # each term, build a corresponding Sympy term and add it to
+    # subising.
+    subising = 0
     for var, weight in bqm.linear.items():
-        subqubo += weight * symbols[var]
+        subising += weight * symbols[var]
 
     for (var1, var2), strength in bqm.quadratic.items():
-        subqubo += strength * symbols[var1] * symbols[var2]
+        subising += strength * symbols[var1] * symbols[var2]
+
+    # Now that we have built the Ising model, convert it to a QUBO.
+    # First, list all variables in the Ising model:
+    all_vars = sympy_util.poly_vars(subising)
+
+    # Build a dict that maps spin variables to boolean variables:
+    spin_to_qubo = dict()
+    for var in all_vars:
+        spin_to_qubo[var] = 1 - 2*sympy.symbols(var.name.replace('spin_', ''))
+
+    # Apply the dict of variable substitutions to the Ising model to
+    # generate the QUBO:
+    subqubo = subising.subs(spin_to_qubo)
+
+    # Expand the QUBO so we are left with a sum of products:
+    subqubo = subqubo.expand()
 
     print('\n**************** QUBO for %s ****************\n' % arg)
     print(subqubo)
