7e177658be1155688f0b7a9d084021f2c51d771c
DennyCQ <denny.dahl@coldquanta.com>
2020-12-23 10:24:50 -0700

    Added logic to scan over the samples and validate the samples against the known-to-be-correct solutions in the qubo_answers.pickle file.  Changed the num_reads parameter to 10000.

--- run_dwave.py
+++ run_dwave.py
@@ -15,6 +15,7 @@ else:
 bqm_filename = 'bqm.pickle'
 embedding_filename = 'embedding.pickle'
 result_filename = 'run_dwave.' + run_label + '.pickle'
+answer_filename = 'qubo_answers.pickle'
 
 sampler = DWaveSampler(solver='DW_2000Q_LANL')
 
@@ -33,7 +34,7 @@ except FileNotFoundError:
     sys.exit(1)
 
 sampler = FixedEmbeddingComposite(sampler, embedding)
-response = sampler.sample(bqm, num_reads=1000, chain_strength=2.0)
+response = sampler.sample(bqm, num_reads=10000, chain_strength=2.0)
 
 print(response.first)
 
@@ -48,3 +49,22 @@ try:
 except FileNotFoundError:
     print('error: file "%s" does not exist' % filename)
     sys.exit(1)
+
+try:
+    with open(answer_filename, 'rb') as f:
+        answers_aux = pickle.load(f)
+except FileNotFoundError:
+    print('error: file "%s" does not exist' % filename)
+    sys.exit(1)
+
+
+valid_samples_found = 0
+
+for i in range(len(response.record)):
+    var_dict = {varname: varvalue for varname, varvalue in zip(response.variables, response.record['sample'][i])}
+    answer_match = [var_dict == answer for answer in answers_aux]
+    if any(answer_match):
+        print('valid sample %d : energy=%6.4f occurrences=%d answer=%d' % (i, response.record['energy'][i], response.record['num_occurrences'][i], answer_match.index(True)))
+        valid_samples_found += response.record['num_occurrences'][i]
+
+print('\nNumber of valid samples found = %d\n' % valid_samples_found)
